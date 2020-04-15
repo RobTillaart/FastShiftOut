11699906090842d358f4124ded9a29bdfa6aa948
rob tillaart <rob.tillaart@gmail.com>
2020-04-15 17:44:42 +0200

    0.2.0 version

---
+++ FastShiftOut.h
@@ -0,0 +1,37 @@
+#pragma once
+//
+//    FILE: FastShiftOut.h
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.2.0
+// PURPOSE: shiftout that implements the Print interface
+//    DATE: 2013-08-22
+//     URL: https://github.com/RobTillaart/FastSHiftOut
+//
+
+#include "Arduino.h"
+#include "Print.h"
+
+#define FASTSHIFTOUT_LIB_VERSION		"0.2.0"
+
+class FastShiftOut : public Print
+{
+public:
+    // bitorder = { LSBFIRST, MSBFIRST };
+    FastShiftOut(const uint8_t datapin, const uint8_t clockpin, const uint8_t bitOrder = LSBFIRST);
+    size_t write(const uint8_t data);
+	
+	// overrule bitorder (most optimized).
+    size_t writeLSBFIRST(const uint8_t data);
+    size_t writeMSBFIRST(const uint8_t data);
+
+private:
+    uint8_t _bitorder;
+
+    uint8_t _databit;
+    volatile uint8_t *_dataout;
+
+    uint8_t _clockbit;
+    volatile uint8_t *_clockout;
+};
+
+// -- END OF FILE --
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # FastShiftOut
-Arduino library for (AVR) optimized shiftOut  - e.g. 74HC595
+
+Arduino library for (AVR) optimized shiftOut - e.g. 74HC595
+
+## Description
+
+This class has optimized code for AVR to shift out data faster 
+than the normal shiftOut.
+
+If not a **ARDUINO_ARCH_AVR** or **ARDUINO_ARCH_MEGAAVR** the class falls back 
+to the default shiftOut implementation.
+
+The interface exists of the following functions:
+
+* **size_t write(const uint8_t data);**
+* **size_t writeLSBFIRST(const uint8_t data);** // most optimized
+* **size_t writeMSBFIRST(const uint8_t data);** // most optimized
+
+Furthermore an FastShiftOut object implements the print interface
+so one can also call
+* **FSO.print(any type);** or 
+* **FSO.println(any type);**
+
+to send e.g. a float with 4 digits over the line.
+
+## Operation
+
+See examples
+
