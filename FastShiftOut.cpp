221aab32adb22d19a7e0091929a1d78a44fad39d
rob tillaart <rob.tillaart@gmail.com>
2020-07-05 19:12:59 +0200

    refactor documentation; fix tabs -> spaces

--- FastShiftOut.cpp
+++ FastShiftOut.cpp
@@ -4,105 +4,104 @@
 // VERSION: 0.2.1
 // PURPOSE: shiftout that implements the Print interface
 //    DATE: 2013-08-22
-//     URL: https://github.com/RobTillaart/FastSHiftOut
+//     URL: https://github.com/RobTillaart/FastShiftOut
 //
 
 #include "FastShiftOut.h"
 
 FastShiftOut::FastShiftOut(const uint8_t datapin, const uint8_t clockpin, const uint8_t bitOrder)
 {
-    _bitorder = bitOrder;
-    pinMode(datapin, OUTPUT);
-    pinMode(clockpin, OUTPUT);
+  _bitorder = bitOrder;
+  pinMode(datapin, OUTPUT);
+  pinMode(clockpin, OUTPUT);
 
 #if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_MEGAAVR)
 
-    // uint8_t _datatimer  = digitalPinToTimer(datapin);
-    // if (_datatimer != NOT_ON_TIMER) turnOffPWM(_datatimer); TODO
-    uint8_t _dataport   = digitalPinToPort(datapin);
-    _dataout = portOutputRegister(_dataport);
-    _databit = digitalPinToBitMask(datapin);
+  // uint8_t _datatimer  = digitalPinToTimer(datapin);
+  // if (_datatimer != NOT_ON_TIMER) turnOffPWM(_datatimer); TODO
+  uint8_t _dataport   = digitalPinToPort(datapin);
+  _dataout = portOutputRegister(_dataport);
+  _databit = digitalPinToBitMask(datapin);
 
-    // uint8_t _clocktimer = digitalPinToTimer(clockpin);
-    // if (_clocktimer != NOT_ON_TIMER) turnOffPWM(_clocktimer);
-    uint8_t _clockport  = digitalPinToPort(clockpin);
-    _clockout = portOutputRegister(_clockport);
-    _clockbit = digitalPinToBitMask(clockpin);
-	
+  // uint8_t _clocktimer = digitalPinToTimer(clockpin);
+  // if (_clocktimer != NOT_ON_TIMER) turnOffPWM(_clocktimer);
+  uint8_t _clockport  = digitalPinToPort(clockpin);
+  _clockout = portOutputRegister(_clockport);
+  _clockbit = digitalPinToBitMask(clockpin);
+
 #else   // reference implementation
 
-	// reuse these vars as pin to save some space
-    _databit = datapin;
-    _clockbit = clockpin;
+  // reuse these vars as pin to save some space
+  _databit = datapin;
+  _clockbit = clockpin;
 
 #endif
 }
 
 size_t FastShiftOut::write(const uint8_t data)
 {
-	if (_bitorder == LSBFIRST)
-	{
-		return writeLSBFIRST(data);
-	}
-	return writeMSBFIRST(data);
+  if (_bitorder == LSBFIRST)
+  {
+    return writeLSBFIRST(data);
+  }
+  return writeMSBFIRST(data);
 }
-
 
 #if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_MEGAAVR)
 
 size_t FastShiftOut::writeLSBFIRST(const uint8_t data)
 {
-    uint8_t cbmask1 = _clockbit;
-    uint8_t cbmask2 = ~_clockbit;
-    uint8_t dbmask1 = _databit;
-    uint8_t dbmask2 = ~_databit;
+  uint8_t cbmask1 = _clockbit;
+  uint8_t cbmask2 = ~_clockbit;
+  uint8_t dbmask1 = _databit;
+  uint8_t dbmask2 = ~_databit;
 
-    for (uint8_t i = 0, m = 1; i < 8; i++)
-    {
-        uint8_t oldSREG = SREG;
-        cli();
-        if ((data & m) == 0) *_dataout &= dbmask2;
-        else                 *_dataout |= dbmask1;
-        *_clockout |= cbmask1;
-        *_clockout &= cbmask2;
-        SREG = oldSREG;
-		m <<= 1;
-    }
-    return 1;
+  for (uint8_t i = 0, m = 1; i < 8; i++)
+  {
+    uint8_t oldSREG = SREG;
+    cli();
+    if ((data & m) == 0) *_dataout &= dbmask2;
+    else                 *_dataout |= dbmask1;
+    *_clockout |= cbmask1;
+    *_clockout &= cbmask2;
+    SREG = oldSREG;
+    m <<= 1;
+  }
+  return 1;
 }
 
 size_t FastShiftOut::writeMSBFIRST(const uint8_t data)
 {
-    uint8_t cbmask1 = _clockbit;
-    uint8_t cbmask2 = ~_clockbit;
-    uint8_t dbmask1 = _databit;
-    uint8_t dbmask2 = ~_databit;
+  uint8_t cbmask1 = _clockbit;
+  uint8_t cbmask2 = ~_clockbit;
+  uint8_t dbmask1 = _databit;
+  uint8_t dbmask2 = ~_databit;
 
-    for (uint8_t i = 0, n = 128; i < 8; i++)
-    {
-        uint8_t oldSREG = SREG;
-        cli();
-        if ((data & n) == 0) *_dataout &= dbmask2;
-        else                 *_dataout |= dbmask1;
-        *_clockout |= cbmask1;
-        *_clockout &= cbmask2;
-        SREG = oldSREG;
-        n >>= 1;
-    }
-    return 1;
+  for (uint8_t i = 0, n = 128; i < 8; i++)
+  {
+    uint8_t oldSREG = SREG;
+    cli();
+    if ((data & n) == 0) *_dataout &= dbmask2;
+    else                 *_dataout |= dbmask1;
+    *_clockout |= cbmask1;
+    *_clockout &= cbmask2;
+    SREG = oldSREG;
+    n >>= 1;
+  }
+  return 1;
 }
 
 #else  // reference implementation  // note this has no cli()
 
 size_t FastShiftOut::writeLSBFIRST(const uint8_t data)
 {
-	shiftOut(_databit, _clockbit, LSBFIRST, data);
+  shiftOut(_databit, _clockbit, LSBFIRST, data);
     return 1;
 }
 
 size_t FastShiftOut::writeMSBFIRST(const uint8_t data)
 {
-	shiftOut(_databit, _clockbit, MSBFIRST, data);
+  shiftOut(_databit, _clockbit, MSBFIRST, data);
     return 1;
 }
 
--- FastShiftOut.h
+++ FastShiftOut.h
@@ -5,7 +5,7 @@
 // VERSION: 0.2.1
 // PURPOSE: shiftout that implements the Print interface
 //    DATE: 2013-08-22
-//     URL: https://github.com/RobTillaart/FastSHiftOut
+//     URL: https://github.com/RobTillaart/FastShiftOut
 //
 
 #include "Arduino.h"
@@ -16,22 +16,22 @@
 class FastShiftOut : public Print
 {
 public:
-    // bitorder = { LSBFIRST, MSBFIRST };
-    FastShiftOut(const uint8_t datapin, const uint8_t clockpin, const uint8_t bitOrder = LSBFIRST);
-    size_t write(const uint8_t data);
-	
-	// overrule bitorder (most optimized).
-    size_t writeLSBFIRST(const uint8_t data);
-    size_t writeMSBFIRST(const uint8_t data);
+  // bitorder = { LSBFIRST, MSBFIRST };
+  FastShiftOut(const uint8_t datapin, const uint8_t clockpin, const uint8_t bitOrder = LSBFIRST);
+  size_t write(const uint8_t data);
+
+  // overrule bitorder (most optimized).
+  size_t writeLSBFIRST(const uint8_t data);
+  size_t writeMSBFIRST(const uint8_t data);
 
 private:
-    uint8_t _bitorder;
+  uint8_t _bitorder;
 
-    uint8_t _databit;
-    volatile uint8_t *_dataout;
+  uint8_t _databit;
+  volatile uint8_t *_dataout;
 
-    uint8_t _clockbit;
-    volatile uint8_t *_clockout;
+  uint8_t _clockbit;
+  volatile uint8_t *_clockout;
 };
 
 // -- END OF FILE --
