1ed476ca16834c5663596b241d6e6d8a48cbb897
rob tillaart <rob.tillaart@gmail.com>
2021-05-27 22:52:10 +0200

    fix capital Examples step 1

--- temporary/FastShiftOut_test/FastShiftOut_test.ino
+++ temporary/FastShiftOut_test/FastShiftOut_test.ino
@@ -1,7 +1,7 @@
 //
-//    FILE: fastShiftOut.ino
+//    FILE: FastShiftOut_test.ino
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.01
+// VERSION: 0.1.2
 // PURPOSE: test sketch
 //     URL: https://github.com/RobTillaart/FastShiftOut
 //
@@ -11,20 +11,32 @@
 
 FastShiftOut FSO(12, 13, LSBFIRST);
 
+uint32_t start, duration1, duration2;
+
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.print("example fastShiftOut: "); 
+  Serial.print("example fastShiftOut: ");
   Serial.println(FASTSHIFTOUT_LIB_VERSION);
 
-  Serial.println("\nPerformance - time in us"); 
-  uint32_t start = micros();
+  Serial.println("\nPerformance - time in us");
+  test1();
+  test2();
+  test3();
+
+  Serial.println("\ndone ...\n");
+}
+
+
+void test1()
+{
+  start = micros();
   for (int i = 0; i < 1000; i++)
   {
     FSO.write(0x55);
   }
-  uint32_t duration1 = micros() - start;
+  duration1 = micros() - start;
   Serial.print("FastShiftOut1: ");
   Serial.println(duration1 * 0.001);
 
@@ -34,13 +46,18 @@ void setup()
     FSO.write(0x55);
     FSO.write(0x55);
   }
-  uint32_t duration2 = micros() - start;
+  duration2 = micros() - start;
   Serial.print("FastShiftOut2: ");
   Serial.println(duration2 * 0.001);
   Serial.print("        Delta: ");
   Serial.println((duration2 - duration1) * 0.001);
   Serial.println();
+  delay(100);
+}
 
+
+void test2()
+{
   start = micros();
   for (int i = 0; i < 1000; i++)
   {
@@ -62,17 +79,21 @@ void setup()
   Serial.print("             Delta: ");
   Serial.println((duration2 - duration1) * 0.001);
   Serial.println();
+  delay(100);
+}
 
+
+void test3()
+{
   Serial.println("\nTest print interface");
   start = micros();
-  for (int i=0; i<100; i++)
+  for (int i = 0; i < 100; i++)
   {
     FSO.println("Hello world");
   }
   duration1 = micros() - start;
   Serial.print("println(\"Hello world\"): \t");
   Serial.println(duration1 * 0.01);
-
 
   start = micros();
   for (int i = 0; i < 100; i++)
@@ -92,8 +113,7 @@ void setup()
   duration1 = micros() - start;
   Serial.print("println(3.14159265, 4): \t");
   Serial.println(duration1 * 0.01);
-
-  Serial.println("\ndone ...");
+  delay(100);
 }
 
 
